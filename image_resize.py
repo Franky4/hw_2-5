3549fffb6e6e46dc8abe00c4a98ca765d88e9350
Aleksey Kozlov <mail@reg65.com>
2018-04-03 06:29:21 +1100

    Добавил поддержку ОС Windows

--- image_resize.py
+++ image_resize.py
@@ -1,6 +1,12 @@
 import subprocess
 # import sys
 import os
+from sys import platform
+
+
+def check_os():
+    # print(platform)
+    return platform
 
 
 def check_result_folder(check_dir):
@@ -22,8 +28,16 @@ def copy_all_files(source_dir, remote_dir):
 
 
 def resize_file(source_file, resize_width):
-    process = subprocess.run('sips --resampleWidth {} {}'.format(resize_width, source_file), shell=True,
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    my_os = check_os()
+    if my_os == "linux" or my_os == "linux2":
+        pass
+    elif my_os == "darwin":
+        process = subprocess.run('sips --resampleWidth {} {}'.format(resize_width, source_file), shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    elif my_os == "win32":
+        process = subprocess.run('convert {} -resize {} {}'.format(source_file, resize_width, source_file), shell=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     # print('ARGV', process.args)
     # print('STDOUT', str(process.stdout))
     # print('STDERR', process.stderr)
@@ -35,6 +49,7 @@ def main():
     source_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), source_dir)
     result_dir = 'Result'
     result_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), result_dir)
+
     check_result_folder(result_dir)
     copy_all_files(source_dir, result_dir)
 
